3ed8a040cf52a667046c14302d69f56f6d36c20b
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-17 22:33:33 +0000

    Add a toolbar with button and title.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,15 @@
 Ext.setup({
-  onReady: function() {}
+	onReady: function() {
+		
+		var toolbar = new Ext.Toolbar({
+			dock: 'top',
+			title: 'Toolbar',
+			items: [{text: 'Button'}]
+		});
+		
+		new Ext.Panel({
+			fullscreen: true,
+			dockedItems: [toolbar]
+		});
+	}
 });
