b6ece71b2ca88d7f7d365ffa811987bad3f036a9
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-17 20:58:48 +0000

    Right then bottom.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,14 +6,14 @@ Ext.setup({
           items: [],
           dockedItems: [
             {
-                dock: "bottom",
-                html: "A docked item",
-                style: "background-color: #fcc;"
-            },
-            {
                 dock: "right",
                 html: "Another docked item",
                 style: "background-color: #caa;"
+            },
+            {
+                dock: "bottom",
+                html: "A docked item",
+                style: "background-color: #fcc;"
             }
           ]
       })
