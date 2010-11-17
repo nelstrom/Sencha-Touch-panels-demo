9a143cdcd5edaf63cc964ccc91e91fda245b009d
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-17 20:41:25 +0000

    Create blank slate.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,15 +1,3 @@
 Ext.setup({
-	onReady: function() {
-		
-		var toolbar = new Ext.Toolbar({
-			dock: 'top',
-			title: 'Toolbar',
-			items: [{text: 'Button'}]
-		});
-		
-		new Ext.Panel({
-			fullscreen: true,
-			dockedItems: [toolbar]
-		});
-	}
+  onReady: function() {}
 });
