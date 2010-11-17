7a9ff717de63b60bdb054104d701abc1145819b8
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-17 22:21:56 +0000

    Add 6 levels of nested panels.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,24 +1,42 @@
 Ext.setup({
   onReady: function() {
+
+    var dimensions = [340,210,130,80,50,30,20,10];
+    
     new Ext.Panel({
       fullscreen: true,
       items: [{
-        width: 340,
-        height: 210,
+        width: dimensions[0],
+        height: dimensions[1],
         cls: 'panel-1',
-        html: 'Panel',
         dockedItems: [{
           dock: 'right',
-          width: 130,
-          height: 210,
+          width: dimensions[2],
+          height: dimensions[1],
           cls: 'panel-2',
-          html: 'Inner panel',
           dockedItems: [{
             dock: 'top',
-            width: 130,
-            height: 80,
+            width: dimensions[2],
+            height: dimensions[3],
             cls: 'panel-3',
-            html: 'Innermost panel',
+            dockedItems: [{
+              dock: 'left',
+              width: dimensions[4],
+              height: dimensions[3],
+              cls: 'panel-4',
+              dockedItems: [{
+                dock: 'bottom',
+                width: dimensions[4],
+                height: dimensions[5],
+                cls: 'panel-5',
+                dockedItems: [{
+                  dock: 'right',
+                  width: dimensions[6],
+                  height: dimensions[5],
+                  cls: 'panel-6'
+                }]
+              }]
+            }]
           }]
         }]
       }]
