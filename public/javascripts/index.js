4a7cef5a2b1cf31634031d0c856ad13f0b132886
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-17 22:15:36 +0000

    Create nested panel with fixed dimensions.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,13 @@
 Ext.setup({
-  onReady: function() {}
+  onReady: function() {
+    new Ext.Panel({
+      fullscreen: true,
+      items: [{
+        width: 340,
+        height: 210,
+        cls: 'panel-1',
+        html: 'Panel'
+      }]
+    });
+  }
 });
